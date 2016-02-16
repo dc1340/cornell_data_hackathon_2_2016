4b03f7837dd8127093eec715bfc87d418e1aa92e
dc1340 <dc1340@gmail.com>
2016-02-16 02:10:29 -0500

    Last changes before presenting
    yolo

--- R/test_app/server.R
+++ R/test_app/server.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(ggplot2)
 
 # Define server logic for slider examples
 shinyServer(function(input, output) {
@@ -14,16 +15,36 @@ shinyServer(function(input, output) {
       stringsAsFactors=FALSE)
   }) 
   
-
+  selectedBTCvars <- reactive({
+    input$btc_vars
+  })
+  
+  selectedCurrVars <- reactive({
+    input$finex_vars
+  })
+  
+  selectedCurrVars <- reactive({
+    input$finex_vars
+  })
+  
+  currentDateRange <- reactive({
+    as.Date(as.character(input$mydateRange))
+  })
   
   output$myggplot <- renderPlot (function (){
-    cur_dat=scal_dat[ , c(input$agg_vars,'USD_Scaled_Price' )]
+    cur_dat=scal_dat[ , c('Date',selectedBTCvars(),'USD_Scaled_Price' )]
+    #cur_data=subset(cur_data, Date>=currentDateRange()[[1]] && Date<=currentDateRange()[[2]])
     melt_dat=melt(cur_dat, id.vars=c('Date'))
-    p <- ggplot(melt_dat,  aes(x=Date, y=value,col=variable)) + geom_point()
-    
+    p <- ggplot(melt_dat,  aes(x=Date, y=value,col=variable)) +geom_point() 
     print(p)
   })
   
-  
+  output$myggplot2 <- renderPlot (function (){
+    cur_dat=scal_dat[ , c('Date',selectedCurrVars(), 'USD_Scaled_Price')]
+    #cur_data=subset(cur_data, Date>=currentDateRange()[[1]] && Date<=currentDateRange()[[2]])
+    melt_dat=melt(cur_dat, id.vars=c('Date'))
+    p <- ggplot(melt_dat,  aes(x=Date, y=value,col=variable)) +geom_point()
+    print(p)
+  })
   
 })
--- R/test_app/ui.R
+++ R/test_app/ui.R
@@ -4,29 +4,32 @@ library(shiny)
 shinyUI(fluidPage(
   
   #  Application title
-  titlePanel("Bitcoin Versus Selected Features Over Time"),
+  titlePanel("Scaled BTCx Versus Selected Scaled Features Over Time"),
   
   # Sidebar with sliders that demonstrate various available
   # options
   sidebarLayout(
     sidebarPanel(
 
-      dateRangeInput("mydateRange", "Date:", "2009-01-03","2016-02-14"),
+      #dateRangeInput("mydateRange", "Date:", "2009-01-03","2016-02-14"),
       checkboxGroupInput('btc_vars', 'Selected Bitcoin Vars', 
-      choices=c("Bitcoin-Average-Transaction-Confirmation-Time"="ATRCT",
-        "Bitcoin-Average-Block-Size"="AVBLS",
-        "Bitcoin-Estimated-Transaction-Volume"="ETRAV",
-        "Bitcoin-Number-of-Unique-Bitcoin-Addresses-Used"="NADDU",
-        "Bitcoin-Number-of-Transactions"="NTRAN",
-        "Bitcoin-Number-of-Transaction-per-Block"="NTRBL",
-        "Bitcoin-Number-of-Transactions-Excluding-Popular-Addresses"="NTREP",
-        "Bitcoin-Total-Output-Volume"="TOUTV",
-        "Bitcoin-Total-Transaction-Fees"="TRFEE"))
+        choices=c("Bitcoin-Average-Transaction-Confirmation-Time"="ATRCT",
+            "Bitcoin-Average-Block-Size"="AVBLS",
+            "Bitcoin-Estimated-Transaction-Volume"="ETRAV",
+            "Bitcoin-Number-of-Unique-Bitcoin-Addresses-Used"="NADDU",
+            "Bitcoin-Number-of-Transactions"="NTRAN",
+            "Bitcoin-Number-of-Transaction-per-Block"="NTRBL",
+            "Bitcoin-Number-of-Transactions-Excluding-Popular-Addresses"="NTREP",
+            "Bitcoin-Total-Output-Volume"="TOUTV")),
+      
+      checkboxGroupInput('finex_vars', 'Selected Currency Exchange Rates', 
+                         choices=c("JPY_Rate","GBP_Rate","CHF_Rate","EUR_Rate","CNY_Rate","CAD_Rate","SEK_Rate"))
     ),
     
     # Show a table summarizing the values entered
     mainPanel(
       plotOutput("myggplot"),
+      plotOutput("myggplot2")
       
     )
   )
